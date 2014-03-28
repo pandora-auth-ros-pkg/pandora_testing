7df565b4d62afeb45889d6120887dc0f118dca6f
Triantafyllos Afouras <afourast@gmail.com>
2014-03-28 03:00:13 +0200

    converted to rostest. ISSUE with rospy subscriber callback catching exception

--- node_tests/scripts/mocks/generic_mock.py
+++ node_tests/scripts/mocks/generic_mock.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-import roslib; roslib.load_manifest('node_tests')
+PKG = 'node_tests'
+import roslib; 
 import rospy
 import actionlib
 import unittest
@@ -7,29 +8,27 @@ import mox
 from time import sleep
 import rosbag
 import sys
-#~ import messageInfoParser
 import mocksubscriber
 import moxcomparators
-import bcolors
 
 from node_tests_msgs.msg import *
 
 class SubscriberMockUnitTest(unittest.TestCase):
 
   def setUp(self):
-    self.person_mocker = mox.Mox()
+    self.mocker = mox.Mox()
     
   def testUsingMox(self):
     
-    dao = self.person_mocker.CreateMock(mocksubscriber.mockSubscriber)
+    mock_obj = self.mocker.CreateMock(mocksubscriber.mockSubscriber)
     
     for topic, msg, t in rosbag.Bag(bagfile).read_messages():    
       if topic in topicsExact :
         compList = []
-        dao.callMethod(moxcomparators.msgEquals(msg))
+        mock_obj.callMethod(moxcomparators.msgEquals(msg))
         
-    self.person_mocker.ReplayAll()
-    self.rsub = mocksubscriber.realSubscriber(topicsExact, messageTypeObj, dao)
+    self.mocker.ReplayAll()
+    self.rsub = mocksubscriber.realSubscriber(topicsExact, messageTypeObj, mock_obj)
     
     client = actionlib.SimpleActionClient('/replay_bags', ReplayBagsAction)
     client.wait_for_server()
@@ -40,32 +39,26 @@ class SubscriberMockUnitTest(unittest.TestCase):
     client.send_goal(goal)
     client.wait_for_result()
     
-    self.person_mocker.VerifyAll()
+    self.mocker.VerifyAll()
     
     rospy.loginfo('exiting')
 
 
 if __name__ == '__main__':
   
-  argv = rospy.myargv(argv=sys.argv)
-  
-  messagePackage = argv[1]
-  messageType = argv[2]
-  bagfile = argv[3]
-  topicsExact = argv[4]
-  
-  #~ pathToMsgDeclaration = argv[5]
-
-  _temp = __import__(messagePackage+'.msg', globals(), locals(), messageType, -1)
-  messageTypeObj = getattr(_temp, messageType)
-
-  #~ messageGoal = messageType[:-10]+messageType[-4:]
-  #~ fieldList = messageInfoParser.parseMessage(pathToMsgDeclaration + '/msg/' + messageGoal + '.msg')
-
-  #~ for i in fieldList:
-    #~ print i
+    import rostest
     
-  rospy.init_node('tester')
-  suite = unittest.TestLoader().loadTestsFromTestCase(SubscriberMockUnitTest)
-  unittest.TextTestRunner(verbosity=2).run(suite)
+    argv = rospy.myargv(argv=sys.argv)
+  
+    messagePackage = argv[1]
+    messageType = argv[2]
+    bagfile = argv[3]
+    topicsExact = argv[4]
+    
+    _temp = __import__(messagePackage+'.msg', globals(), locals(), messageType, -1)
+    messageTypeObj = getattr(_temp, messageType)
+    
+    rospy.init_node('tester')
+    rostest.rosrun(PKG, 'test_using_mox', SubscriberMockUnitTest) 
+
   
